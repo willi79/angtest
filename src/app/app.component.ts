041454aaedcd59cd1870c4882a9ea9c098796587
willi79 <willigeraldy@gmail.com>
2020-02-06 13:46:24 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,11 @@ declare var $: any;
 export class AppComponent implements OnInit {
   title = 'website';
   
+  
   ngOnInit(){
+
+    var dropdownstatus="False";
+
     $(".icon_yotang").click(
       function(){
         alert(" I Love U Nouvelle!!!!!!!!!! "); 
@@ -23,33 +27,132 @@ export class AppComponent implements OnInit {
 
     $(".option_about").mouseenter(
       function() {
-        $("#op1").css('display', 'block');
+        $(".op1").css('display', 'inline-block');
       }).mouseleave(
         function() {
-          $("#op1").css('display', 'none');
-        }
-      );
-  
-
-      $(".option_yotang").mouseenter(
+          $(".op1").css('display', 'none');
+        });
+        
+    $(".op1").mouseenter(
+      function() {
+        $(".op1").css('display', 'inline-block');
+      }).mouseleave(
         function() {
-          $("#op2").css('display', 'block');
-        }).mouseleave(
-          function() {
-            $("#op2").css('display', 'none');
-          }
-        );
+          $(".op1").css('display', 'none');
+        }
+    );
+    $(".option_yotang").mouseenter(
+      function() {
+        $(".op2").css('display', 'inline-block');
+      }).mouseleave(
+        function() {
+          $(".op2").css('display', 'none');
+        });
+        
+    $(".op2").mouseenter(
+      function() {
+        $(".op2").css('display', 'inline-block');
+      }).mouseleave(
+        function() {
+          $(".op2").css('display', 'none');
+        }
+    );
+
   
           
-  $(".dropdownmain").click(
+    $(".dropdownmain").click(
+      
       function() {
+        $(".dropdownmain").css('display', 'none');
+        $("#dropdownmain1").css('display', 'block');
+        $("#dropdownmain2").css('display', 'none');
         $("#dropdownitems").css('display', 'block');
-      });
+      }
+      );
+    
+    $("#dropdownmain1").click(
+    
+      function() {
+        $(".dropdownmain").css('display', 'block');
+        $("#dropdownmain1").css('display', 'none');
+        $("#dropdownmain2").css('display', 'none');
+        $("#dropdownitems").css('display', 'none');
+        dropdownstatus="True";
+      }
+      );
   
-      $(".dropdown").mouseleave(
+    
+
+    $("#dropdownmain2").click(
+  
+      function() {
+        $("#dropdownmain1").css('display', 'block');
+        $("#dropdownmain2").css('display', 'none');
+        $(".up_class").css('display','block');
+        $(".up1_class").css('display','none'); 
+        $(".up2_class").css('display','none'); 
+        $(".up_class_hr").css('display','block');
+        $(".up1_class_hr").css('display','none'); 
+        $(".up2_class_hr").css('display','none'); 
+        $("#op2_3_mobile_dd_main").css('display','none'); 
+        $(".op2_3_mobile_dd").css('display','none'); 
+        dropdownstatus="True";
+      }
+      );
+
+    $(".dropdown").mouseleave(
+      function() {
+        $(".dropdownmain").css('display', 'block');
+        $("#dropdownmain1").css('display', 'none');
+        $("#dropdownmain2").css('display', 'none');
+        $("#dropdownitems").css('display', 'none');
+        $(".up_class").css('display','block');
+        $(".up1_class").css('display','none'); 
+        $(".up2_class").css('display','none'); 
+        $(".up_class_hr").css('display','block');
+        $(".up1_class_hr").css('display','none'); 
+        $(".up2_class_hr").css('display','none'); 
+        $("#op2_3_mobile_dd_main").css('display','none'); 
+        $(".op2_3_mobile_dd").css('display','none'); 
+      });
+
+    $("#op1_mobile").click(
+      function() {
+        $(".up_class").css('display','none');
+        $(".up1_class").css('display','block'); 
+        $(".up_class_hr").css('display','none');
+        $(".up1_class_hr").css('display','block'); 
+        $("#dropdownmain1").css('display', 'none');
+        $("#dropdownmain2").css('display', 'block');
+      });
+
+      $("#op2_mobile").click(
         function() {
-          $("#dropdownitems").css('display', 'none');
+          $(".up_class").css('display','none');
+          $(".up2_class").css('display','block'); 
+          $(".up_class_hr").css('display','none');
+          $("#op2_3_mobile_dd_main").css('display','none');
+          $(".up2_class_hr").css('display','block'); 
+          $("#dropdownmain1").css('display', 'none');
+          $("#dropdownmain2").css('display', 'block');
+          $(".op2_3_mobile_dd_hr").css('display','block');
         });
+
+      $("#op2_3_mobile").click(
+        function() {
+          $("#op2_3_mobile").css('display','none');
+          $("#op2_3_mobile_dd_main").css('display','block'); 
+          $(".op2_3_mobile_dd_hr").css('display','block');
+          $(".op2_3_mobile_dd").css('display','block'); 
+
+        });
+    $("#op2_3_mobile_dd_main").click(
+      function() {
+        $("#op2_3_mobile").css('display','block');
+        $("#op2_3_mobile_dd_main").css('display','none'); 
+        $(".op2_3_mobile_dd").css('display','none'); 
+
+      });
 
   }
 
